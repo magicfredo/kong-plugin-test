16345114521d725dbe215504b92429215a5cd23f
Frédéric Mersseman <fmersseman@ankama.com>
2018-02-27 15:36:52 +0100

    test

--- src/tools.lua
+++ src/tools.lua
@@ -1,13 +1,12 @@
-object_classic = require "classic"
+local Object = require "kong.vendor.classic"
+local Tools = Object:extend()
 
-local tools = object_classic:extend()
-
-function tools:new(api_name)
+function Tools:new(api_name)
     self._api_name = api_name
 end
 
-function tools:get_api_name()
+function Tools:get_api_name()
     return self._api_name
 end
 
-return tools
+return Tools
