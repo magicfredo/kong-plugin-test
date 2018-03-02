7abfeb8e242c8567cb59eed6b145420fb6d351ab
Frédéric Mersseman <fmersseman@ankama.com>
2018-03-02 09:58:57 +0100

    test

--- src/access.lua
+++ src/access.lua
@@ -7,31 +7,7 @@ local responses    = require 'kong.tools.responses'
 local singletons   = require "kong.singletons"
 
 local jwt_secret_private = [[-----BEGIN RSA PRIVATE KEY-----
-MIIEogIBAAKCAQEApCafAPfjZL7IaOm7E+UuAiT4YmmNJx2UC9amggGcCXyOcZUC
-loZlfjl1PFtmvorDdNWwvtl239EBuEPFJ3uyxmabFOFHAI3TzDEIxmDRbF89Zgwp
-KlCNN+fy05+FFJ/ee7guh8hNOaFGjhoWO3CdB40oAMYb4ThjLdo9c0HARBxXVEHe
-0V2o/Zh7lysMdZzThhghMLG4O4poDkPR7nNcRRsHchqvH9PyQsYRgEj/q2QG607Z
-kd98cZO6aAHwBZ23YyC4+Xf1M6ahcmkagmfsa4qerjKT5t1tGbmg+ap7ukdaT/F/
-6AzyS3+gMacgHBmyC9gK8HfPWlZg9fLBdOX43wIDAQABAoIBACQc4wpBfuNhHZIH
-KcMcvlx2abas5gLe/P0FwigD1qI3ptbSI3meI6H7XhFjQl1VBd8kS/gQu5hN0QUK
-08r16cTAOgoD7azvhtL7TqTmE+y1nzpeHohSpF7SRpfMFmkNKtMpGAODS4oMMwt8
-Lbvx8HoC0fQpoWSRFTC3PKGoq3XQNByYdxW9iI8wEt8ash+CjFtQao2csoTot8f9
-KQhdeSaGp8E1uM7s2kDRi1n/g5t/RDxp09ofUxfqAZL9RjmKPdrHLLqt7zCF7nva
-CYRNaJkYmNepse/iu7eEVYno4KLANNP7mVTuHb5mycKTK/I5xfwyXfBaPMhKhszS
-srjqoEECgYEA1H/sNnaYyEdZsYA4ETXgt/qiWRBju/PmxYTzgOrctw96+RKhacAd
-HfQCTXx9jz6yihAxUnylb/Z8lP6cXWyPD3Nfmki28zIv29PgT2OwtToEefN/mu+W
-odTsFjXR/9pFe7vzEssaJN8K3blmYVfwph2EjtHoNl0s1OOE4sOo/c8CgYEAxcD4
-hHwRSuCiZa28u+3McfIlCB3nNrz7TLqWJhblxdggtUcfUiV7XNqcfMCFawtgkACL
-AP5fR0PsC290QLHV9JZIkXcSb/CuymrJC1qJGyqK5y8PRe0Sq7HkbZINNVIpJvtl
-EM9m4GCfFgnwTZpVSi68s7eTTl7do/JxSXszp/ECgYB5SwtpiwhqSU/JIYbfTAGZ
-Albov5IuFmoDFIBpdaXGV++5fAjmc+Iq1rz5vbVtrjv60oNUshE8d1VlNm/KY8zE
-5PYM+rRy3JK0x5uhtSWITDWB5ptPtLImbYLUqqPThqNinUWB1Kx3n1h0dv8ZUTjr
-mK2xV99UKJsOaU/QoB41wQKBgE4/iRaDMSR7tkaddGy4L4l20whfLLoQFS/LNNZl
-gQ3D801HkzEh+6pGJl2GoEQ3AEJ6tIX0ISdFzQTJWSqwM4TQYm6MuxLoHYGit2Jy
-tIW3U7cee+CjahveDBD/FZLfq8DtAJSiPIbUNJ632Aoc41qzG5/RJ8x+5RyWNhVp
-VotBAoGAQD+g8TsNdMEYNxsL/FNLgU+PzhnuJLtVzDQyUyuHZ42Onjvbtr9H7VSQ
-J4I6J32qu/abUE7uAYH9JkQIG85Nw93SR/sdP5eNObKZAjOfI6L5igeyDJe2+u9a
-jgJsOwJ1/6bTKyknvYLIH8MJhGo8A+sj1FvF5WoemAoEjDOkDnw=
+...
 -----END RSA PRIVATE KEY-----]]
 
 local token_ankama_ms = [[COPY_AND_PASTE]]
@@ -48,15 +24,23 @@ function _M.execute(conf)
     ak_tools:new() -- conf.api_name
     ngx.header['X-Ankama-Api-Name'] = ak_tools:get_api_name()
 
-    ak_tools:print(ngx.req.get_uri_args(), 'GET_URI_ARGS')
+    ak_tools:print(ngx.req, 'NGX.REQ')
 
     local result_set, err = singletons.dao.tbl_ankama:insert({
         username = 'magicfredo',
         consumer_id = '0c9d0a88-0e58-446a-b062-50082167de86'
     }, {ttl = 300})
 
-    ak_tools:print(err, 'CASSANDRA ERR')
-    ak_tools:print(result_set, 'CASSANDRA RESULT')
+    ak_tools:print(err, 'INSERT_ERR')
+    ak_tools:print(result_set, 'INSERT_RESULT')
+
+    local result, err = singletons.dao.tbl_ankama:find({
+        username = 'magicfredo'
+    })
+
+    ak_tools:print(err, 'FIND_ERR')
+    ak_tools:print(result_set, 'FIND_RESULT')
+
 
 
     local authorization_type;
